28c9e2e2c0e9b09810043328c1f1ded773d8b8a8
andreacataluffi <andrea.cataluffi@e-lios.eu>
2025-08-29 19:51:47 +0200

    fix

--- src/main/java/org/unicam/intermediate/models/enums/TaskType.java
+++ src/main/java/org/unicam/intermediate/models/enums/TaskType.java
@@ -30,5 +30,8 @@ public enum TaskType {
         return null;
     }
 
-
+    @Override
+    public String toString() {
+        return xmlValue;
+    }
 }
--- src/main/java/org/unicam/intermediate/service/EvaluationService.java
+++ src/main/java/org/unicam/intermediate/service/EvaluationService.java
@@ -2,20 +2,18 @@ package org.unicam.intermediate.service;
 
 import lombok.AllArgsConstructor;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
-import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
-import org.springframework.stereotype.Component;
 import org.unicam.intermediate.service.environmental.TemperatureService;
 import org.unicam.intermediate.utils.BooleanEvaluation.BooleanExpressionEvaluator;
 import org.unicam.intermediate.utils.BooleanEvaluation.PlaceExpressionParser;
 
 @AllArgsConstructor
-public class DecisioneService {
+public class EvaluationService {
 
-    private final ProcessEngineConfigurationImpl config;
+    private final TemperatureService temperatureService;
 
     public void evaluatePlaceCondition(DelegateExecution execution, String expressionText, String outputvariableName) {
         PlaceExpressionParser.ConditionParts cp =  PlaceExpressionParser.parse(expressionText);
-        double temperature = new TemperatureService().getTemperatureFromPlace(cp.placeId);
+        double temperature = temperatureService.getTemperatureFromPlace(cp.placeId);
         double threshold = Double.parseDouble(cp.value);
         var evaluation = BooleanExpressionEvaluator.evaluate(temperature, cp.operator, threshold);
         execution.setVariable(outputvariableName, evaluation);
